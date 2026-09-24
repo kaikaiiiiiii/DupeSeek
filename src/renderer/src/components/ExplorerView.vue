08b12b86c2d15c4dec5f2baea3224e7347b8b31f
kaikaiiiiiii <yourkaikai@gmail.com>
2026-09-25 04:43:50 +0800

    style: prettier

--- src/renderer/src/components/ExplorerView.vue
+++ src/renderer/src/components/ExplorerView.vue
@@ -92,9 +92,7 @@
 
     <div class="footer-bar">
       <span class="footer-tip">拖动条目到左侧列表，或点选后批量添加</span>
-      <button class="btn scan-settings-btn" @click="emit('open-scan-settings')">
-        扫描设置
-      </button>
+      <button class="btn scan-settings-btn" @click="emit('open-scan-settings')">扫描设置</button>
     </div>
   </div>
 </template>
